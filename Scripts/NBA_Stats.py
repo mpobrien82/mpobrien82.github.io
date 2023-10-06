d3044fa86fb5a4c0ebcd4b5ab417215c02fda11e
mpobrien82 <146033671+mpobrien82@users.noreply.github.com>
2023-10-06 09:21:56 -0700

    Update NBA_Stats.py

--- Scripts/NBA_Stats.py
+++ Scripts/NBA_Stats.py
@@ -1,42 +1,44 @@
 import gspread
 from google.oauth2 import service_account
 import pandas as pd
+from flask import Flask, render_template_string
+
+app = Flask(__name__)
 
 def fetch_data(sheet_name):
-    # Set up Google Sheets API credentials
-    credentials = service_account.Credentials.from_service_account_file(
-        'C:/Users/mpobr/Downloads/nba-player-prop-model-fcab08224af6.json',
-        scopes=["https://www.googleapis.com/auth/spreadsheets"]
-    )
+    # Your existing code for fetching data from Google Sheets
+    # ...
 
-    # Authenticate with the Google Sheets API using the credentials
-    gc = gspread.Client(auth=credentials)
+@app.route('/')
+def display_data():
+    sheet1_name = 'Dashboard'
+    data_frame1 = fetch_data(sheet1_name)
 
-    # URL of your Google Sheets document
-    url = 'https://docs.google.com/spreadsheets/d/1SwQJeR8mxvJS31wgYs4gY7h6OUuP7Ogk2Mi9c-gb2v4/edit#gid=700047262'
+    # Convert the DataFrame to an HTML table
+    table_html = data_frame1.to_html(classes='table table-bordered table-hover', index=False)
 
-    # Open the specific sheet by URL
-    doc = gc.open_by_url(url)
+    # HTML template for your static website
+    html_template = """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>My Static Website</title>
+        <style>
+            /* Add any CSS styling here */
+        </style>
+    </head>
+    <body>
+        <h1>Data from Google Sheets</h1>
+        {{ table_html | safe }}
+    </body>
+    </html>
+    """
 
-    # Select the specific worksheet by title (using the specified sheet_name)
-    worksheet = doc.worksheet(sheet_name)
+    # Render the template with the data
+    rendered_template = render_template_string(html_template, table_html=table_html)
 
-    # Retrieve data from the worksheet
-    data = worksheet.get_all_values()
+    return rendered_template
 
-    # Convert the data to a Pandas DataFrame
-    sheet_df = pd.DataFrame(data[1:], columns=data[0])
+if __name__ == '__main__':
+    app.run(debug=True)
 
-    # Now 'sheet_df' contains the data as a DataFrame
-    return sheet_df
-
-# Specify the names of the sheets you want to fetch
-sheet1_name = 'Dashboard'
-
-# Call the fetch_data function with the specified sheet names to fetch the data as DataFrames
-data_frame1 = fetch_data(sheet1_name)
-
-
-# Print the first few rows of the DataFrames to verify the data
-print("Data from Sheet 1:")
-print(data_frame1.head())
