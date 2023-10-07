ef93d2c5edb28f931f64dcc584643fdcea94013a
mpobrien82 <146033671+mpobrien82@users.noreply.github.com>
2023-10-06 21:18:14 -0700

    Create build.sh

---
+++ Scripts/build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Create a virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Install required Python packages
+pip install -r requirements.txt
+
+# Run your Python script
+python Scripts/GDoc_Scrape.py
